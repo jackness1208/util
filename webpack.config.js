8c49e859a0fbb3d19c2e305982fc269094d58995
jackNEss <jackness1208@qq.com>
2016-05-12 08:21:24 +0800

    [EDIT] 添加 qunit 单元测试

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@
 module.exports = {
     //页面入口文件配置
     entry: {
-        pc : './src/pc/util.pc.js',
-        mobile : './src/mobile/util.mobile.js'
+        'util.pc' : './src/pc/util.pc.js',
+        'util.mobile' : './src/mobile/util.mobile.js'
     },
     //入口文件输出配置
     output: {
